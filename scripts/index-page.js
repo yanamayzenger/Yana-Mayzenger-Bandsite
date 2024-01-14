10ecd0e92bc2261caaebc12b5bbe30812ed09f46
Yana Mayzenger <55948608+yanamayzenger@users.noreply.github.com>
2024-01-14 14:47:50 -0500

    first version of the Comments from api

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,75 +1,71 @@
-const form = document.querySelector(".comments__input-form");
+const bandSiteApi = new BandSiteApi("cf7bbab3-9fa7-40a2-a428-1207b967000d");
+
+const form = document.querySelector(".comment__input-container");
 
 function displayComments(arr) {
-  const commentDiv = document.querySelector(".comments__section");
+  // Selector updated to match HTML class
+  const commentDiv = document.querySelector(".comment__default-comment");
   commentDiv.innerHTML = "";
 
-  for (let comment of arr) {
-    const mainDiv = document.createElement("div");
-    mainDiv.classList.add("comments__new");
+  arr.forEach((comment) => {
+    let m = new Date(comment.timestamp);
+    let dateString = `${
+      m.getUTCMonth() + 1
+    }/${m.getUTCDate()}/${m.getUTCFullYear()}`;
 
-    const subDiv = document.createElement("div");
-    subDiv.classList.add("comments__container");
-    mainDiv.appendChild(subDiv);
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("comment__default");
+
+    const headerDiv = document.createElement("div");
+    headerDiv.classList.add("comment__header");
+
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("comment__header--image-one");
+    headerDiv.appendChild(imageDiv);
 
     const mainName = document.createElement("h2");
     mainName.innerText = comment.name;
-    mainName.classList.add("comments__new-name");
-    subDiv.appendChild(mainName);
-
-    let m = new Date(comment.timestamp);
-    let dateString =
-      m.getUTCMonth() + 1 + "/" + m.getUTCDate() + "/" + m.getUTCFullYear();
+    mainName.classList.add("comment__header--name");
+    headerDiv.appendChild(mainName);
 
     const mainTime = document.createElement("aside");
     mainTime.innerText = dateString;
-    mainTime.classList.add("comments__new-time");
-    subDiv.appendChild(mainTime);
+    mainTime.classList.add("comment__header--date");
+    headerDiv.appendChild(mainTime);
+
+    mainDiv.appendChild(headerDiv);
 
     const mainP = document.createElement("p");
     mainP.innerText = comment.comment;
-    mainP.classList.add("comments__new-comment");
-    subDiv.appendChild(mainP);
+    mainP.classList.add("comment__text-container-default--comment");
+    mainDiv.appendChild(mainP);
 
     commentDiv.appendChild(mainDiv);
-  }
+  });
 }
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+(async () => {
+  try {
+    const comments = await bandSiteApi.getComments();
+    displayComments(comments);
+  } catch (error) {
+    console.error("Error loading comments:", error);
+  }
+})();
 
-  const name = event.target.querySelector(".comments__input-info-name").value;
-  const comment = event.target.querySelector(
-    ".comments__input-info-comment"
-  ).value;
+form.addEventListener("submit", async (newPost) => {
+  newPost.preventDefault();
+  try {
+    const newComment = {
+      name: document.querySelector(".comment__text-container--one").value,
+      comment: document.querySelector(".comment__text-container--two").value,
+    };
 
-  if (name && comment) {
-    axios
-      .post(
-        "https://project-1-api.herokuapp.com/comments?api_key=cf7bbab3-9fa7-40a2-a428-1207b967000d",
-        {
-          name: name,
-          comment: comment,
-        }
-      )
-      .then(() => {
-        getComments();
-        event.target.reset();
-      });
+    await bandSiteApi.postComment(newComment);
+    const updatedComments = await bandSiteApi.getComments();
+    displayComments(updatedComments);
+    form.reset();
+  } catch (error) {
+    console.error("Error posting a new comment:", error);
   }
 });
-
-function getComments() {
-  axios
-    .get(
-      "https://project-1-api.herokuapp.com/comments?api_key=cf7bbab3-9fa7-40a2-a428-1207b967000d"
-    )
-    .then((response) => {
-      displayComments(
-        response.data.sort(function (a, b) {
-          return b.timestamp - a.timestamp;
-        })
-      );
-    });
-}
-getComments();
