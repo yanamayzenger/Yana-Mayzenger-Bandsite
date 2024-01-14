2dc6f0e5b7ea122ef1f95b3f0c3dc8e8567434bc
Yana Mayzenger <55948608+yanamayzenger@users.noreply.github.com>
2024-01-14 14:49:39 -0500

    fixed styling for index page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,40 +1,29 @@
 const bandSiteApi = new BandSiteApi("cf7bbab3-9fa7-40a2-a428-1207b967000d");
-
 const form = document.querySelector(".comment__input-container");
-
 function displayComments(arr) {
-  // Selector updated to match HTML class
   const commentDiv = document.querySelector(".comment__default-comment");
   commentDiv.innerHTML = "";
-
   arr.forEach((comment) => {
     let m = new Date(comment.timestamp);
     let dateString = `${
       m.getUTCMonth() + 1
     }/${m.getUTCDate()}/${m.getUTCFullYear()}`;
-
     const mainDiv = document.createElement("div");
     mainDiv.classList.add("comment__default");
-
     const headerDiv = document.createElement("div");
     headerDiv.classList.add("comment__header");
-
     const imageDiv = document.createElement("div");
     imageDiv.classList.add("comment__header--image-one");
     headerDiv.appendChild(imageDiv);
-
     const mainName = document.createElement("h2");
     mainName.innerText = comment.name;
     mainName.classList.add("comment__header--name");
     headerDiv.appendChild(mainName);
-
     const mainTime = document.createElement("aside");
     mainTime.innerText = dateString;
     mainTime.classList.add("comment__header--date");
     headerDiv.appendChild(mainTime);
-
     mainDiv.appendChild(headerDiv);
-
     const mainP = document.createElement("p");
     mainP.innerText = comment.comment;
     mainP.classList.add("comment__text-container-default--comment");
@@ -43,7 +32,6 @@ function displayComments(arr) {
     commentDiv.appendChild(mainDiv);
   });
 }
-
 (async () => {
   try {
     const comments = await bandSiteApi.getComments();
@@ -52,7 +40,6 @@ function displayComments(arr) {
     console.error("Error loading comments:", error);
   }
 })();
-
 form.addEventListener("submit", async (newPost) => {
   newPost.preventDefault();
   try {
@@ -60,7 +47,6 @@ form.addEventListener("submit", async (newPost) => {
       name: document.querySelector(".comment__text-container--one").value,
       comment: document.querySelector(".comment__text-container--two").value,
     };
-
     await bandSiteApi.postComment(newComment);
     const updatedComments = await bandSiteApi.getComments();
     displayComments(updatedComments);
